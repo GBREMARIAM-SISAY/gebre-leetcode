6c44aad153d73982d29b55239aad105c6f6f3155
GBREMARIAM-SISAY <117940674+GBREMARIAM-SISAY@users.noreply.github.com>
2023-03-14 13:54:14 +0300

    Time: 64 ms (85.27%), Space: 44 MB (76.95%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let myarr=[]
+    
+    for(let i=1; i<=n ; i++ ){
+        let str=''
+        if (i%3===0 && i%5===0){
+            str+='FizzBuzz'
+        }else if(i%3===0){
+           str+='Fizz'
+        }else if(i%5===0){
+            str+='Buzz'
+        }else {
+           str+=`${i}`
+    }
+       myarr.push(str) 
+   }
+    return myarr
+};
